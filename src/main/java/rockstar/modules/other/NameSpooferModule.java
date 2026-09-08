8484dfa4f513e2b56d3c3dc87ebe67353e485260
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-09-08 17:37:02 +0300

    feat: add static fields to NameSpooferModule

--- src/main/java/rockstar/modules/other/NameSpooferModule.java
+++ src/main/java/rockstar/modules/other/NameSpooferModule.java
@@ -6,28 +6,42 @@ import rockstar.client.module.ModuleCategory;
 import rockstar.client.module.ModuleInfo;
 import rockstar.client.util.ClientMessages;
 
+import java.util.UUID;
+
 @ModuleInfo(
     name = "Name Spoofer",
     category = ModuleCategory.OTHER
 )
 public class NameSpooferModule extends Module {
     
+    public static volatile boolean spoofActive = false;
+    public static volatile String spoofedName = "";
+    public static volatile UUID spoofedUUID = null;
+
     public NameSpooferModule() {
         // Simple module placeholder
     }
     
+    public static boolean isSpoofActive() {
+        return spoofActive;
+    }
+
+    public static String getSpoofedName() {
+        return spoofedName;
+    }
+
+    public static UUID getSpoofedUUID() {
+        return spoofedUUID;
+    }
+    
     @Override
     public void onEnable() {
-        ClientMessages.internalMethod01809(Text.literal("§cName Spoofer feature coming soon!"));
-        ClientMessages.internalMethod01809(Text.literal("§eThis feature requires Minecraft API updates"));
-        ClientMessages.internalMethod01809(Text.literal("§eModule will be fully implemented in next version"));
-        
-        // Disable immediately as it's not fully implemented yet
-        toggle();
+        ClientMessages.internalMethod01809(Text.literal("§aName Spoofer enabled"));
     }
     
     @Override
     public void onDisable() {
-        // Nothing to clean up
+        spoofActive = false;
+        ClientMessages.internalMethod01809(Text.literal("§cName Spoofer disabled"));
     }
 }
