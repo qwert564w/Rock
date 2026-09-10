5a56877564b6883ed3a06bf69291f0896a9d6808
$CR1PT <sashadegtevMMXII@yandex.ru>
2026-09-10 20:11:50 +0300

    refactor: NameSpoofMixin to use SpoofManager

--- src/main/java/moscow/rockstar/mixin/NameSpoofMixin.java
+++ src/main/java/moscow/rockstar/mixin/NameSpoofMixin.java
@@ -6,20 +6,14 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import rockstar.modules.other.NameSpooferModule;
-
-import java.util.UUID;
+import rockstar.modules.other.spoof.SpoofManager;
 
 @Mixin(PlayerEntity.class)
 public class NameSpoofMixin {
     @Inject(method = "getGameProfile", at = @At("HEAD"), cancellable = true)
     private void onGetGameProfile(CallbackInfoReturnable<GameProfile> cir) {
-        if (NameSpooferModule.isSpoofActive()) {
-            String spoofedName = NameSpooferModule.getSpoofedName();
-            UUID spoofedUUID = NameSpooferModule.getSpoofedUUID();
-            if (spoofedName != null && spoofedUUID != null) {
-                cir.setReturnValue(new GameProfile(spoofedUUID, spoofedName));
-            }
+        if (SpoofManager.isSpoofing && SpoofManager.targetName != null && SpoofManager.targetUUID != null) {
+            cir.setReturnValue(new GameProfile(SpoofManager.targetUUID, SpoofManager.targetName));
         }
     }
 }
